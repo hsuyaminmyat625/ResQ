212f3106f6a5a1952e640b9e2d212f2b3eabd90a
Hsu Yamin Myat <hsuyaminmyat625@users.noreply.github.com>
2025-11-19 10:39:05 +0900

    Initial Commit

---
+++ ResQ/ResQApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResQApp.swift
+//  ResQ
+//
+//  Created by sym on 2025/11/19.
+//
+
+import SwiftUI
+
+@main
+struct ResQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
